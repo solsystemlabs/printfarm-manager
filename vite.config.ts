e785f961e834e0c5d7d4b801e9c8d04fc72a0dca
teernisse <teernisse@gmail.com>
2025-10-28 21:49:48 -0400

    fix deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import { defineConfig } from "vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 import viteReact from "@vitejs/plugin-react";
+import netlify from "@netlify/vite-plugin-tanstack-start";
 
 export default defineConfig(() => {
   return {
@@ -14,6 +15,7 @@ export default defineConfig(() => {
       }),
       tanstackStart(),
       viteReact(),
+      netlify(),
     ],
   };
 });
